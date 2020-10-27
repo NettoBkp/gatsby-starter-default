f90d4441095fca99eabf25816eb90fe5ce4ba7a7
NettoBkp <artsystemti@gmail.com>
2020-10-27 07:33:27 +0000

    s

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -8,7 +8,7 @@ const SecondPage = () => (
   <Layout>
     <SEO title="Page two" />
     <h1>Hi from the second pagess</h1>
-    <p>Welcome to pageas  2s</p>
+    <p>Welcome to pageas s 2s</p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
