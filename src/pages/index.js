183251cf8decaa7694a969059392b78074034dcc
NettoBkp <artsystemti@gmail.com>
2020-10-28 05:14:05 +0000

    init netlify login

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,20 +4,56 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import { Component } from "react"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <h2>Login</h2>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
+function iniNetlifyIdentify() {
+    onmouseleave.log("deu ruim")
+    const string = document.createElement("script");
+
+    script.src = "https://identity.netlify.com/v1/netlify-identify-widget.js"
+    script.async = true;
+
+    document.body.appendChild(script);
+}
+
+function openNetlifyModal() {
+    const netlifyIdentity = window.netlifyIdentity;
+
+    if (netlifyIdentity)
+        netlifyIdentity.open();
+    else
+        console.log('netfilyIdentify not define')
+}
+
+class NetlifyIdentity extends Component {
+    componentDidMount() {
+        iniNetlifyIdentify();
+    }
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+const IndexPage = () => {
+
+    return (
+        <Layout>
+            <SEO title="Home" />
+            <h1>Hi people</h1>
+            <h2>Login</h2>
+            <p>Welcome to your new Gatsby site.</p>
+            <p>Now go build something great.</p>
+            <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+                <Image />
+            </div>
+            <Link to="/page-2/">Go to page 2</Link> <br />
+            <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+        </Layout>
+    )
+}
 
 export default IndexPage
